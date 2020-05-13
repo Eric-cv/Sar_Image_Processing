a5a3a0eb9708f3763970f59d13d68020c08a890a
Eric-cv <zhangqi_@pku.edu.cn>
2020-05-14 00:24:03 +0800

    new project of sar image processing

---
+++ script.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+# @Time     :2020/5/13 0013 10:44
+# @Author   :Eric
+# @Email    :zhangqi_@pku.edu.cn
+# @File     :script.py
+# @Software :PyCharm
+
+import numpy as np
+import cv2, torch
+from matplotlib import pyplot as plt
+
+
+# c h w
+# im = cv2.imread(r'images\laneseg.jpg')
+# im = cv2.imread(r'images\20180718-20180724\dint_geocode.png')
+# im = cv2.imread(r'images\label.png')
+im = cv2.imread(r'images\car.png')
+# im1 = im.swapaxes(0, 0)
+print(type(im), im.shape, im.dtype)
+cv2.imshow('test_color', im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# plt.figure(figsize=(20, 8), dpi=50)
+# plt.imshow(im)
+# plt.show()
+
+'''
+# c h w
+# arr = np.array((range(28 * 3)).reshape(4, 7, 3))
+# arr[3,6,:]=
+arr = np.zeros((5, 5))
+
+plt.figure(figsize=(20, 8), dpi=50)
+plt.imshow(arr)
+plt.show()
+print(arr)
+# brr=np.array(range(28)).reshape((7,4))
+# print(arr, brr, sep='\n')
+# print(arr.shape, brr.shape)
+'''
