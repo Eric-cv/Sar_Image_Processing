a657588c64202e55b38b87ce54954895fbc18153
Eric-cv <zhangqi_@pku.edu.cn>
2020-05-14 10:59:24 +0800

    save code normally

--- script.py
+++ script.py
@@ -9,15 +9,16 @@ import numpy as np
 import cv2, torch
 from matplotlib import pyplot as plt
 
-
 # c h w
 # im = cv2.imread(r'images\laneseg.jpg')
 # im = cv2.imread(r'images\20180718-20180724\dint_geocode.png')
-# im = cv2.imread(r'images\label.png')
-im = cv2.imread(r'images\car.png')
-# im1 = im.swapaxes(0, 0)
+im = cv2.imread(r'images\label.png')
+# im = cv2.imread(r'images\car.png')X
+mask = np.zeros(im.shape[:2], np.uint8)
+mask[im == 128] = 255
 print(type(im), im.shape, im.dtype)
-cv2.imshow('test_color', im)
+cv2.imshow('mask', mask)
+cv2.imwrite('./mask.png', mask)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 # plt.figure(figsize=(20, 8), dpi=50)
