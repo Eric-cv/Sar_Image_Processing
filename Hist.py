fe8fdbe94039d83918815fa510528af8d567dca1
Eric-cv <zhangqi_@pku.edu.cn>
2020-05-21 23:56:43 +0800

    image hist code finished

--- Hist.py
+++ Hist.py
@@ -8,18 +8,35 @@
 import cv2
 from matplotlib import pyplot as plt
 
-img_path = r'images/20180718-20180724/dint_geocode.png'
-lab_path = r'./images/label.png'
-# img_path = r'images/20180805-20180823/dint_geocode1.png'
-# lab_path = r'./images/label1.png'
-# lab_path = r'H:/ZQ_file/Sar_seg/new_labels/dint_geocode_mask.png'
-# lab_path = r'H:/ZQ_file/Sar_seg/new_labels/dint_geocode_mask1.png'
+# img_path = r'images/cat.jpg'
+img_path1 = r'images/20180718-20180724/dint_geocode.png'
+img_path2 = r'images/20180718-20180724/coh_geocode.png'
 
-img = cv2.imread(img_path)
+img1 = cv2.imread(img_path1)
+img2 = cv2.imread(img_path2)
+
 color = ('b', 'g', 'r')
+plt.figure(figsize=(10, 8), dpi=80)
+ax1 = plt.subplot(211)
 for i, col in enumerate(color):
-    hist = cv2.calcHist([img], [i], None, [256], [0, 256])
-    # plt.figsize()
-    plt.plot(hist, color=col)
+    hist1 = cv2.calcHist([img1], [i], None, [256], [0, 256])
+
+    plt.plot(hist1, color=col)
     plt.xlim([0, 256])
-    plt.show()
+    plt.grid(alpha=0.8)
+    plt.xlabel('pixel value')
+    plt.ylabel('pixel quantity')
+    plt.title('Hist of dint_geocode')
+
+ax2 = plt.subplot(212)
+for i, col in enumerate(color):
+    hist2 = cv2.calcHist([img2], [i], None, [256], [0, 256])
+    # fig = plt.figure(figsize=(20, 8), dpi=80)
+    plt.plot(hist2, color=col)
+    plt.xlim([0, 256])
+    plt.grid(alpha=0.8)
+    plt.xlabel('pixel value')
+    plt.ylabel('pixel quantity')
+    plt.title('Hist of coh_geocode')
+
+plt.show()
